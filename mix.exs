b703074b0b3a02c11087cc9e4ec849227bedeeb1
Chris Tierney <notproperlycut@gmail.com>
2022-08-22 19:18:53 +0000

    wip

--- mix.exs
+++ mix.exs
@@ -7,22 +7,33 @@ defmodule EcstaticDemo.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      mod: {EcstaticDemo.Application, []},
+      extra_applications: [:logger],
+      start_phases: [ configure: []]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      #{:ecstatic, git: "https://github.com/notproperlycut/ecstatic.git", branch: "bootstrap/handlers"}
+      {:ecstatic, path: "../ecstatic"}
+    ]
+  end
+
+  defp aliases do
+    [
+      up: ["ecstatic.up"],
+      down: ["ecstatic.down"],
     ]
   end
 end
